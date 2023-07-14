fb01ea00e79d2dd27359fbc4f84fcfdb31e1834e
lynnagidza <lynnagidza@gmail.com>
2023-07-14 16:43:56 +0300

    corrections

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Async Generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """ A coroutine that will loop 10 times,
         each time asynchronously wait 1 second,
         then yield a random number between 0 and 10.
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Async Comprehensions """
 import asyncio
+from typing import Generator
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> float:
+async def measure_runtime() -> Generator[float, None, None]:
     """ Coroutine will execute async_comprehension four times in parallel
         using asyncio.gather.
         measure_runtime should measure the total runtime and return it.
