97f86cea0a42acc6de180ff6a77aeff37509f578
lynnagidza <lynnagidza@gmail.com>
2023-07-17 13:16:34 +0300

    fixed task 1s pycodestyle & and 2s function return value annotation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ Async Comprehensions """
 import asyncio
-from typing import Generator
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> Generator[float, None, None]:
+async def measure_runtime() -> float:
     """ Coroutine will execute async_comprehension four times in parallel
         using asyncio.gather.
         measure_runtime should measure the total runtime and return it.
